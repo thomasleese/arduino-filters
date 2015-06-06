c3f4b9629ed44742727726b6ca7cc4038c926e9e
Tom Leese <inbox@tomleese.me.uk>
2015-06-06 12:40:59 +0100

    Add a complementary filter

--- src/Filters.cpp
+++ src/Filters.cpp
@@ -7,3 +7,16 @@
 #include <Arduino.h>
 
 #include "Filters.h"
+
+
+using namespace Filters;
+
+
+Complementary::Complementary(double proportion) {
+    mProportion = proportion;
+    mOppositeProportion = 1.0 - proportion;
+}
+
+double Complementary::apply(double value1, double value2) const {
+    return mProportion * value1 + mOppositeProportion * value2;
+}
--- src/Filters.h
+++ src/Filters.h
@@ -7,6 +7,21 @@
 #ifndef FILTERS_H
 #define FILTERS_H
 
+namespace Filters {
 
+    class Complementary {
+
+    public:
+        Complementary(double proportion);
+
+        double apply(double value1, double value2) const;
+
+    private:
+        double mProportion;
+        double mOppositeProportion;
+
+    };
+
+}
 
 #endif // FILTERS_H
